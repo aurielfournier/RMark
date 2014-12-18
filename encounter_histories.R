896dc68829bcd084c9ff481528d4ca61dedb493a
Auriel Fournier <aurielfournier@gmail.com>
2014-12-18 13:43:23 -0600

    basic encounter history works, still need to incorporate live_dead component

--- encounter_histories.R
+++ encounter_histories.R
@@ -1,18 +1,19 @@
 # Making Encounter Histories
-setwd("~/RMark")
 library(reshape)
-dat <- read.csv('direct_recoveries_cago_ar_banded_2002_2011_metal_only.csv',header=T)
-dat <- dat[,c("band","b_year","r_year","b_month","r_month")]
-dat$b_new_year <- ifelse(dat$b_month>=6,dat$b_year, dat$b_year+1)
-dat$r_new_year <- ifelse(dat$r_month>=6,dat$r_year, dat$r_year+1)
-b <- dat[,c("band","b_new_year")]
-colnames(b) <- c("band","year")
-r <- dat[,c("band","r_new_year")]
-colnames(r) <- c("band","year")
-rat <- rbind(b,r)
+setwd("/Users/AurielFournier/Documents/RMark")
+dat <- read.csv("canada_geese_arkansas.csv",header=T)
+dat$b_new_year <- ifelse(dat$banding_month>=6,dat$banding_year, dat$banding_year-1)
+dat$e_new_year <- ifelse(dat$encounter_month>=6,dat$encounter_year, dat$encounter_year-1)
+b <- dat[,c("band_id","b_new_year","l_d","b_age_code","b_sex_code")]
+colnames(b) <- c("band_id","year","l_d","age","sex")
+e <- dat[,c("band_id","e_new_year","l_d","b_age_code","b_sex_code")]
+colnames(e) <- c("band_id","year","l_d","age","sex")
+rat <- rbind(b,e)
 rat$num <- 1
+rat <- rat[rat$year>=2000,]
+cdat <- cast(data=rat, band_id ~ year)
 
-cdat <- cast(data=rat, band ~ year)
+# ----------
 cdat[2:ncol(cdat)] <- ifelse(cdat[2:ncol(cdat)]>=1, 1,0)
 
 cdat$cap <- paste(cdat$"2002", cdat$"2003",cdat$"2004",cdat$"2005",cdat$"2006",cdat$"2007",cdat$"2008",cdat$"2009",cdat$"2010",cdat$"2011",cdat$"2012", sep="")
