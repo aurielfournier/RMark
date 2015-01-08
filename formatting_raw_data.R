5045b898d4196844ff2591c10e5677dd53e80272
Auriel Fournier <aurielfournier@gmail.com>
2015-01-08 10:27:26 -0600

    push to work computer at end of break

--- formatting_raw_data.R
+++ formatting_raw_data.R
@@ -2,8 +2,8 @@
 
 setwd("~/Documents/RMark")
 dat1 <- read.csv('Fournier_bnds_CAGO_AR_201412180839.csv',header=T)
-#dat2 <- read.csv('Fournier_encs_from_AR_2000_201412180934.csv',header=T)
-dat3 <- read.csv('Fournier_encs_to_AR_201412181101.csv',header=T)
+#dat2 <- read.csv('Fournier_encs_from_AR_2000_201412180934.csv',header=T) #Birds banded in AR, recovered elsewhere
+dat3 <- read.csv('Fournier_encs_to_AR_201412181101.csv',header=T) #birds recovered in AR, banded anywhere
 
 dat1$state <- "AR"
 dat1 <- dat1[dat1$SPECIES_ID==1720,] #removes the cackling geese. 
@@ -16,9 +16,10 @@ dat1$e_present_code <- 7
 
 #dat2 <- dat2[,c("BAND_NUM","B_AGE_CODE","B_SEX_CODE","BANDING_DATE","BANDING_DAY","BANDING_MONTH","BANDING_YEAR","E_PLACE_NAME","ENCOUNTER_DATE","ENCOUNTER_DAY","ENCOUNTER_MONTH","ENCOUNTER_YEAR","E_PRESENT_CONDITION_CODE")]
 
-dat3 <- dat3[dat3$E.HOW.OBTAINED.CODE==50|dat3$E.HOW.OBTAINED.CODE==56|dat3$E.HOW.OBTAINED.CODE==70|dat3$E.HOW.OBTAINED.CODE==98,]
-dat3 <- dat3[dat3$ENCOUNTER.DAY<=31,]
-dat3 <- dat3[dat3$B.AREA.CODE==7,]
+dat3 <- dat3[dat3$E.HOW.OBTAINED.CODE!=50&dat3$E.HOW.OBTAINED.CODE!=56&dat3$E.HOW.OBTAINED.CODE!=70&dat3$E.HOW.OBTAINED.CODE!=98,]
+# those four codes were told not to be included by the BBL in survival analysis
+dat3 <- dat3[dat3$ENCOUNTER.DAY<=31,] # removes impossible dates
+dat3 <- dat3[dat3$B.AREA.CODE==7,] #7 == Arkansas
 dat3$state <- "AR"
 dat3 <- dat3[,c("BAND.ID","B.AGE.CODE","B.SEX.CODE","BANDING.DATE","BANDING.DAY","BANDING.MONTH","BANDING.YEAR","state","ENCOUNTER.DATE","ENCOUNTER.DAY","ENCOUNTER.MONTH","ENCOUNTER.YEAR","E.PRESENT.CONDITION.CODE")]
 colnames(dat3) <- tolower(colnames(dat3))
