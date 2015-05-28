f1cfcfdfdedbb7129fdd47007d93ac5caf4d6637
aurielfournier <aurielfournier@gmail.com>
2015-05-28 16:22:06 -0500

    incorporated CAGO AR live captures, moving 4 rivers along

--- encounter_histories_for_program_mark.R
+++ encounter_histories_for_program_mark.R
@@ -1,7 +1,7 @@
 # Making Encounter Histories
 library(reshape)
-setwd("~/RMark")
-dat <- read.csv("canada_geese_arkansas.csv",header=T)
+
+dat <- read.csv("~/GitHub/RMark/canada_geese_arkansas.csv",header=T)
 dat$b_new_year <- ifelse(dat$banding_month>=6,dat$banding_year, dat$banding_year-1)
 dat$e_new_year <- ifelse(dat$encounter_month>=6,dat$encounter_year, dat$encounter_year-1)
 b <- dat[,c("band_id","b_new_year","l_d","b_age_code","b_sex_code")]
@@ -12,14 +12,12 @@ rat <- rbind(b,e)
 rat$num <- 1
 rat <- rat[rat$year>=2005,]
 rat <- rat[rat$year<=2011,]
-
-
-
+rat <- rat[!(is.na(rat$year)),]
 # Regular Encounter History ---------------------------------------------------
 
 cdat <- cast(data=rat, band_id + age + sex ~ year)
 cdat[4:ncol(cdat)] <- ifelse(cdat[4:ncol(cdat)]>=1, 1,0)
-cdat$cap <- paste(cdat$"2000",cdat$"2001",cdat$"2002", cdat$"2003",cdat$"2004",cdat$"2005",cdat$"2006",cdat$"2007",cdat$"2008",cdat$"2009",cdat$"2010",cdat$"2011",cdat$"2012",cdat$"2013",cdat$"2014", sep="")
+cdat$cap <- paste(cdat$"2005",cdat$"2006",cdat$"2007",cdat$"2008",cdat$"2009",cdat$"2010",cdat$"2011", sep="")
 enhist <- data.frame(cdat$band_id, cdat$sex , cdat$age, cdat$cap)
 enhist$cdat.cap <- as.character(enhist$cdat.cap)
 colnames(enhist) <- c("band","sex","age","ch")
@@ -33,32 +31,37 @@ rat <- rat[!(is.na(rat$l_d)),] # remove the NA's
 cdat <- cast(data=rat, band_id ~ year + l_d)
 sex <- cast(data=rat, band_id ~ sex)
 age <- cast(data=rat, band_id ~ age)
-
 cdat[2:ncol(cdat)] <- ifelse(cdat[2:ncol(cdat)]>=1, 1,0)
-
-cdat[,4:ncol(cdat)][cdat$"2005_dead"==1,]<-0
-cdat[,6:ncol(cdat)][cdat$"2006_dead"==1,]<-0
-cdat[,8:ncol(cdat)][cdat$"2007_dead"==1,]<-0
-cdat[,10:ncol(cdat)][cdat$"2008_dead"==1,]<-0
-cdat[,12:ncol(cdat)][cdat$"2009_dead"==1,]<-0
-cdat[,14:ncol(cdat)][cdat$"2010_dead"==1,]<-0
-
 
 
 cdat <- cbind(cdat, sex[,2:3])
 cdat <- cbind(cdat, age[,2:4])
 cdat[2:ncol(cdat)] <- ifelse(cdat[2:ncol(cdat)]>=1, 1,0)
 
-
-
 colnames(cdat) <- c("band_id","2005_alive","2005_dead","2006_alive","2006_dead","2007_alive","2007_dead","2008_alive","2008_dead","2009_alive","2009_dead","2010_alive","2010_dead","2011_alive","2011_dead","male","female","ahy","hy","local")
+
+
+
+
+
+cdat[,4:15][cdat$"2005_dead"==1,]<-0
+cdat[,6:15][cdat$"2006_dead"==1,]<-0
+cdat[,8:15][cdat$"2007_dead"==1,]<-0
+cdat[,10:15][cdat$"2008_dead"==1,]<-0
+cdat[,12:15][cdat$"2009_dead"==1,]<-0
+cdat[,14:15][cdat$"2010_dead"==1,]<-0
+
 #create capture history
 cdat$cap <- paste(cdat$"2005_alive",cdat$"2005_dead",cdat$"2006_alive",cdat$"2006_dead",cdat$"2007_alive",cdat$"2007_dead",cdat$"2008_alive",cdat$"2008_dead",cdat$"2009_alive",cdat$"2009_dead",cdat$"2010_alive",cdat$"2010_dead",cdat$"2011_alive",cdat$"2011_dead", " ", cdat$male, " ", cdat$female," ",cdat$ahy," ",cdat$hy," ", cdat$local, ";", sep="")
+
+
 # below removes birds banded before 2005 that were never recovered during the study period
 cdat <- cdat[!(cdat$"2005_alive"==0&cdat$"2006_alive"==0&cdat$"2007_alive"==0&cdat$"2008_alive"==0&cdat$"2009_alive"==0&cdat$"2010_alive"==0&cdat$"2011_alive"==0),]
 
 
+
 enhist <- data.frame(cdat$band_id, cdat$cap)
+colnames(enhist) <- c("band_id","ch")
 
 
-write.table(enhist[,2], 'live_dead_enhist_clean.inp', row.names=F, col.names=F, quote=F)
+write.table(enhist[,2], '~/GitHub/RMark/live_dead_enhist_clean.inp', row.names=F, col.names=F, quote=F)
--- formatting_raw_data.R
+++ formatting_raw_data.R
@@ -1,9 +1,31 @@
 # Manipulating Raw Data Files from the BBL 
 
-setwd("~/Documents/RMark")
+
 dat1 <- read.csv('Fournier_bnds_CAGO_AR_201412180839.csv',header=T)
 #dat2 <- read.csv('Fournier_encs_from_AR_2000_201412180934.csv',header=T) #Birds banded in AR, recovered elsewhere
 dat3 <- read.csv('Fournier_encs_to_AR_201412181101.csv',header=T) #birds recovered in AR, banded anywhere
+dat2 <- read.csv('agfc_banding_00_11.csv')
+
+colnames(dat2) <- c("band_id","e_present_condition_code","spp","b_age_code","b_sex_code","banding_month","banding_day","banding_year")
+dat2$state <- "AR"
+dat2$encounter_date <- NA
+dat2$encounter_day <- NA
+dat2$encounter_month <- NA
+dat2$encounter_year <- NA
+
+dat2$b_sex_code <- ifelse(dat2$b_sex_code=="M",4,
+                          ifelse(dat2$b_sex_code=="F",5,0))
+
+dat2$b_age_code <- ifelse(dat2$b_age_code=="AHY",1,
+                          ifelse(dat2$b_age_code=="HY",2,
+                                 ifelse(dat2$b_age_code=="AA",1,
+                                        ifelse(dat2$b_age_code=="L",4,
+                                               ifelse(dat2$b_age_code=="A",1,NA)))))
+
+dat2 <- dat2[!(is.na(dat2$b_age_code)),]
+
+
+dat2 <- dat2[,c("band_id","b_age_code","b_sex_code","banding_day","banding_month","banding_year","state","encounter_day","encounter_month","encounter_year","e_present_condition_code")]
 
 dat1$state <- "AR"
 dat1 <- dat1[dat1$SPECIES_ID==1720,] #removes the cackling geese. 
@@ -30,12 +52,18 @@ colnames(dat3) <- namesdat3
 colnames(dat1) <- colnames(dat3)
 #colnames(dat2) <- colnames(dat3)
 
-dat1_2 <- rbind(dat1, dat3)
+dat1 <- dat1[,c("band_id","b_age_code","b_sex_code","banding_day","banding_month","banding_year","state","encounter_day","encounter_month","encounter_year","e_present_condition_code")]
+dat3 <- dat3[,c("band_id","b_age_code","b_sex_code","banding_day","banding_month","banding_year","state","encounter_day","encounter_month","encounter_year","e_present_condition_code")]
+
+dat1_2<- rbind(dat1, dat3)
 #dat1_2 <- rbind(dat1_1, dat3)
 
+dat2$l_d <- "alive"
 dat1_2$l_d <- ifelse(dat1_2$e_present_condition_code==1|dat1_2$e_present_condition_code==2|dat1_2$e_present_condition_code==3,"unknown",
                   ifelse(dat1_2$e_present_condition_code==4|dat1_2$e_present_condition_code==5|dat1_2$e_present_condition_code==6,"dead",
                         ifelse(dat1_2$e_present_condition_code==7|dat1_2$e_present_condition_code==8|dat1_2$e_present_condition_code==9|dat1_2$e_present_condition_code==10|dat1_2$e_present_condition_code==11|dat1_2$e_present_condition_code==12|dat1_2$e_present_condition_code==13|dat1_2$e_present_condition_code==14|dat1_2$e_present_condition_code==15|dat1_2$e_present_condition_code==16,"alive",NA)))
 
 dat1_2 <- dat1_2[dat1_2$banding_year>=2000,]
+
+dat <- rbind(dat2, dat1_2)
 write.csv(dat1_2, "canada_geese_arkansas.csv", row.names=F)
